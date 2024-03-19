d922b837a4086d0b9ccf579dbb1ca49a4b5c0caa
unknown <mahmoudelboshy@gmail.com>
2024-03-19 22:39:04 +0200

    Initiate

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\BaseApiController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends BaseApiController
+{
+    //
+}
